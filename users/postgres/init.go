e7a4904b97bcec8c9adddcfb0693747ef7db31db
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-12-15 11:35:53 +0100

    remove owner id
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- users/postgres/init.go
+++ users/postgres/init.go
@@ -78,6 +78,13 @@ func migrateDB(db *sqlx.DB) error {
 					`ALTER TABLE IF EXISTS users ADD PRIMARY KEY (id)`,
 				},
 			},
+			{
+				Id: "users_5",
+				Up: []string{
+					`ALTER TABLE IF EXISTS users DROP CONSTRAINT IF EXISTS users_owner_id_fkey`,
+					`ALTER TABLE IF EXISTS users DROP COLUMN IF EXISTS owner_id`,
+				},
+			},
 		},
 	}
 
