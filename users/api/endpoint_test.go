102cf097b8c64b554b5e5d4a28d3f216900ad8e9
mteodor <mirko.teodorovic@gmail.com>
2022-03-24 21:27:35 +0100

    fix tests
    Signed-off-by: mteodor <mirko.teodorovic@gmail.com>

--- users/api/endpoint_test.go
+++ users/api/endpoint_test.go
@@ -16,6 +16,8 @@ import (
 	"testing"
 
 	"github.com/mainflux/mainflux"
+	"github.com/mainflux/mainflux/internal/apiutil"
+	"github.com/mainflux/mainflux/logger"
 	"github.com/mainflux/mainflux/pkg/errors"
 	"github.com/mainflux/mainflux/pkg/uuid"
 	"github.com/mainflux/mainflux/users"
@@ -39,14 +41,19 @@ const (
 )
 
 var (
-	user           = users.User{Email: validEmail, Password: validPass}
-	admin          = users.User{Email: adminEmail, Password: validPass}
-	notFoundRes    = toJSON(errorRes{errors.ErrNotFound.Error()})
-	unauthRes      = toJSON(errorRes{errors.ErrAuthentication.Error()})
-	malformedRes   = toJSON(errorRes{errors.ErrMalformedEntity.Error()})
-	weakPassword   = toJSON(errorRes{users.ErrPasswordFormat.Error()})
-	unsupportedRes = toJSON(errorRes{errors.ErrUnsupportedContentType.Error()})
-	passRegex      = regexp.MustCompile("^.{8,}$")
+	user               = users.User{Email: validEmail, Password: validPass}
+	admin              = users.User{Email: adminEmail, Password: validPass}
+	notFoundRes        = toJSON(errorRes{errors.ErrNotFound.Error()})
+	unauthRes          = toJSON(errorRes{errors.ErrAuthentication.Error()})
+	malformedRes       = toJSON(errorRes{errors.ErrMalformedEntity.Error()})
+	weakPassword       = toJSON(errorRes{users.ErrPasswordFormat.Error()})
+	unsupportedRes     = toJSON(errorRes{errors.ErrUnsupportedContentType.Error()})
+	missingTokRes      = toJSON(apiutil.ErrorRes{Err: apiutil.ErrBearerToken.Error()})
+	missingEmailRes    = toJSON(apiutil.ErrorRes{Err: apiutil.ErrMissingEmail.Error()})
+	missingPassRes     = toJSON(apiutil.ErrorRes{Err: apiutil.ErrMissingPass.Error()})
+	invalidRestPassRes = toJSON(apiutil.ErrorRes{Err: apiutil.ErrInvalidResetPass.Error()})
+	failDecodeRes      = toJSON(apiutil.ErrorRes{Err: errors.ErrMalformedEntity.Error()})
+	passRegex          = regexp.MustCompile("^.{8,}$")
 )
 
 type testRequest struct {
@@ -64,7 +71,7 @@ func (tr testRequest) make() (*http.Response, error) {
 		return nil, err
 	}
 	if tr.token != "" {
-		req.Header.Set("Authorization", tr.token)
+		req.Header.Set("Authorization", apiutil.BearerPrefix+tr.token)
 	}
 	if tr.contentType != "" {
 		req.Header.Set("Content-Type", tr.contentType)
@@ -91,7 +98,7 @@ func newService() users.Service {
 }
 
 func newServer(svc users.Service) *httptest.Server {
-	mux := api.MakeHandler(svc, mocktracer.New())
+	mux := api.MakeHandler(svc, mocktracer.New(), logger.NewMock())
 	return httptest.NewServer(mux)
 }
 
@@ -110,6 +117,50 @@ func TestRegister(t *testing.T) {
 	invalidData := toJSON(users.User{Email: invalidEmail, Password: validPass})
 	invalidPasswordData := toJSON(users.User{Email: validEmail, Password: invalidPass})
 	invalidFieldData := fmt.Sprintf(`{"email": "%s", "pass": "%s"}`, user.Email, user.Password)
+
+	cases := []struct {
+		desc        string
+		req         string
+		contentType string
+		status      int
+		token       string
+	}{
+		{"register new user", data, contentType, http.StatusCreated, ""},
+		{"register existing user", data, contentType, http.StatusConflict, ""},
+		{"register user with invalid email address", invalidData, contentType, http.StatusBadRequest, ""},
+		{"register user with weak password", invalidPasswordData, contentType, http.StatusBadRequest, ""},
+		{"register user with invalid request format", "{", contentType, http.StatusBadRequest, ""},
+		{"register user with empty JSON request", "{}", contentType, http.StatusBadRequest, ""},
+		{"register user with empty request", "", contentType, http.StatusBadRequest, ""},
+		{"register user with invalid field name", invalidFieldData, contentType, http.StatusBadRequest, ""},
+		{"register user with missing content type", data, "", http.StatusUnsupportedMediaType, ""},
+	}
+
+	for _, tc := range cases {
+		req := testRequest{
+			client:      client,
+			method:      http.MethodPost,
+			url:         fmt.Sprintf("%s/register", ts.URL),
+			contentType: tc.contentType,
+			token:       tc.token,
+			body:        strings.NewReader(tc.req),
+		}
+		res, err := req.make()
+		assert.Nil(t, err, fmt.Sprintf("%s: unexpected error %s", tc.desc, err))
+		assert.Equal(t, tc.status, res.StatusCode, fmt.Sprintf("%s: expected status code %d got %d", tc.desc, tc.status, res.StatusCode))
+	}
+}
+
+func TestUserCreate(t *testing.T) {
+	svc := newService()
+	ts := newServer(svc)
+	defer ts.Close()
+	client := ts.Client()
+
+	data := toJSON(user)
+	invalidData := toJSON(users.User{Email: invalidEmail, Password: validPass})
+	invalidPasswordData := toJSON(users.User{Email: validEmail, Password: invalidPass})
+	invalidFieldData := fmt.Sprintf(`{"email": "%s", "pass": "%s"}`, user.Email, user.Password)
 	unauthzEmail := "unauthz@example.com"
 
 	cases := []struct {
@@ -119,18 +170,18 @@ func TestRegister(t *testing.T) {
 		status      int
 		token       string
 	}{
-		{"register new user", data, contentType, http.StatusCreated, admin.Email},
-		{"register user with empty token", data, contentType, http.StatusForbidden, ""},
-		{"register existing user", data, contentType, http.StatusConflict, admin.Email},
-		{"register user with invalid email address", invalidData, contentType, http.StatusBadRequest, admin.Email},
-		{"register user with weak password", invalidPasswordData, contentType, http.StatusBadRequest, admin.Email},
-		{"register new user with unauthorized access", data, contentType, http.StatusForbidden, unauthzEmail},
-		{"register existing user with unauthorized access", data, contentType, http.StatusForbidden, unauthzEmail},
-		{"register user with invalid request format", "{", contentType, http.StatusBadRequest, admin.Email},
-		{"register user with empty JSON request", "{}", contentType, http.StatusBadRequest, admin.Email},
-		{"register user with empty request", "", contentType, http.StatusBadRequest, admin.Email},
-		{"register user with invalid field name", invalidFieldData, contentType, http.StatusBadRequest, admin.Email},
-		{"register user with missing content type", data, "", http.StatusUnsupportedMediaType, admin.Email},
+		{"create new user", data, contentType, http.StatusCreated, admin.Email},
+		{"create user with empty token", data, contentType, http.StatusForbidden, ""},
+		{"create existing user", data, contentType, http.StatusConflict, admin.Email},
+		{"create user with invalid email address", invalidData, contentType, http.StatusBadRequest, admin.Email},
+		{"create user with weak password", invalidPasswordData, contentType, http.StatusBadRequest, admin.Email},
+		{"create new user with unauthorized access", data, contentType, http.StatusUnauthorized, unauthzEmail},
+		{"create existing user with unauthorized access", data, contentType, http.StatusUnauthorized, unauthzEmail},
+		{"create user with invalid request format", "{", contentType, http.StatusBadRequest, admin.Email},
+		{"create user with empty JSON request", "{}", contentType, http.StatusBadRequest, admin.Email},
+		{"create user with empty request", "", contentType, http.StatusBadRequest, admin.Email},
+		{"create user with invalid field name", invalidFieldData, contentType, http.StatusBadRequest, admin.Email},
+		{"create user with missing content type", data, "", http.StatusUnsupportedMediaType, admin.Email},
 	}
 
 	for _, tc := range cases {
@@ -296,9 +347,9 @@ func TestPasswordResetRequest(t *testing.T) {
 		res         string
 	}{
 		{"password reset request with valid email", data, contentType, http.StatusCreated, expectedExisting},
-		{"password reset request with invalid email", nonexistentData, contentType, http.StatusBadRequest, notFoundRes},
+		{"password reset request with invalid email", nonexistentData, contentType, http.StatusNotFound, notFoundRes},
 		{"password reset request with invalid request format", "{", contentType, http.StatusBadRequest, malformedRes},
-		{"password reset request with empty JSON request", "{}", contentType, http.StatusBadRequest, malformedRes},
+		{"password reset request with empty JSON request", "{}", contentType, http.StatusBadRequest, missingEmailRes},
 		{"password reset request with empty request", "", contentType, http.StatusBadRequest, malformedRes},
 		{"password reset request with missing content type", data, "", http.StatusUnsupportedMediaType, unsupportedRes},
 	}
@@ -372,9 +423,9 @@ func TestPasswordReset(t *testing.T) {
 	}{
 		{"password reset with valid token", reqExisting, contentType, http.StatusCreated, "{}", token},
 		{"password reset with invalid token", reqNoExist, contentType, http.StatusUnauthorized, unauthRes, token},
-		{"password reset with confirm password not matching", reqPassNoMatch, contentType, http.StatusBadRequest, malformedRes, token},
+		{"password reset with confirm password not matching", reqPassNoMatch, contentType, http.StatusBadRequest, invalidRestPassRes, token},
 		{"password reset request with invalid request format", "{", contentType, http.StatusBadRequest, malformedRes, token},
-		{"password reset request with empty JSON request", "{}", contentType, http.StatusBadRequest, malformedRes, token},
+		{"password reset request with empty JSON request", "{}", contentType, http.StatusBadRequest, missingPassRes, token},
 		{"password reset request with empty request", "", contentType, http.StatusBadRequest, malformedRes, token},
 		{"password reset request with missing content type", reqExisting, "", http.StatusUnsupportedMediaType, unsupportedRes, token},
 		{"password reset with weak password", reqPassWeak, contentType, http.StatusBadRequest, weakPassword, token},
@@ -445,10 +496,10 @@ func TestPasswordChange(t *testing.T) {
 		tok         string
 	}{
 		{"password change with valid token", dataResExisting, contentType, http.StatusCreated, "{}", token},
-		{"password change with invalid token", reqNoExist, contentType, http.StatusUnauthorized, unauthRes, ""},
+		{"password change with empty token", reqNoExist, contentType, http.StatusUnauthorized, missingTokRes, ""},
 		{"password change with invalid old password", reqWrongPass, contentType, http.StatusUnauthorized, unauthRes, token},
 		{"password change with invalid new password", reqWeakPass, contentType, http.StatusBadRequest, weakPassword, token},
-		{"password change with empty JSON request", "{}", contentType, http.StatusBadRequest, malformedRes, token},
+		{"password change with empty JSON request", "{}", contentType, http.StatusBadRequest, missingPassRes, token},
 		{"password change empty request", "", contentType, http.StatusBadRequest, malformedRes, token},
 		{"password change missing content type", dataResExisting, "", http.StatusUnsupportedMediaType, unsupportedRes, token},
 	}
