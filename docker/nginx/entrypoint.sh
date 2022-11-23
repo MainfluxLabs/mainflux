c76a5389278a2a5fbc773703af40aa0b735c8642
Manuel Imperiale <manuel.imperiale@gmail.com>
2022-11-23 14:28:12 +0100

    NOISSUE - Fix docker-compose and add ui
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -17,6 +17,9 @@ envsubst '
     ${MF_NGINX_MQTT_PORT}
     ${MF_NGINX_MQTTS_PORT}
     ${MF_AUTH_HTTP_PORT}
-    ${MF_WS_ADAPTER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    ${MF_WS_ADAPTER_PORT}
+    ${MF_UI_PORT}
+    ${MF_INFLUX_READER_PORT}
+    ${MF_FILESTORE_HTTP_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
