0e8c2eb16a395eb6988a54c77b218e45e1730e1e
Manuel Imperiale <manuel.imperiale@gmail.com>
2023-02-28 10:50:08 +0100

    ML-57 - Remove Twins and OPC-UA services
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- twins/mongodb/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Mainflux
-// SPDX-License-Identifier: Apache-2.0
-
-// Package mongodb contains repository implementations using MongoDB as
-// the underlying database.
-package mongodb
