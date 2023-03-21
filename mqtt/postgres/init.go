f4655f65ac670a2fa258c6ce967d1652ce3cc44b
Manuel Imperiale <manuel.imperiale@gmail.com>
2023-03-21 16:42:44 +0100

    Fix indentation
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- mqtt/postgres/init.go
+++ mqtt/postgres/init.go
@@ -46,11 +46,11 @@ func migrateDB(db *sqlx.DB) error {
 				Id: "mqtt_1",
 				Up: []string{
 					`CREATE TABLE IF NOT EXISTS subscriptions (
-							subtopic    VARCHAR(1024),
+					    subtopic    VARCHAR(1024),
 					    channel_id  UUID,
 					    thing_id    UUID,
-							client_id   VARCHAR(256) UNIQUE,
-							status      VARCHAR(128),
+					    client_id   VARCHAR(256) UNIQUE,
+					    status      VARCHAR(128),
 					    created_at  FLOAT,
 					    PRIMARY KEY (subtopic, channel_id, thing_id)
 					)`,
