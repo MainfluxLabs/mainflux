54ebe8f997452da54a2f62beec551567fa21fc80
Zoran Rebic <56149596+zzokki81@users.noreply.github.com>
2023-07-06 02:12:56 +0200

    NOISSUE - Fix update admin id (#226)
    Signed-off-by: zzokki81 <zoran.rebic@outlook.com>

--- users/postgres/users.go
+++ users/postgres/users.go
@@ -81,7 +81,7 @@ func (ur userRepository) Update(ctx context.Context, user users.User) error {
 }
 
 func (ur userRepository) UpdateUser(ctx context.Context, user users.User) error {
-	q := `UPDATE users SET metadata = :metadata WHERE email = :email AND status = 'enabled'`
+	q := `UPDATE users SET id = :id, metadata = :metadata WHERE email = :email AND status = 'enabled'`
 
 	dbu, err := toDBUser(user)
 	if err != nil {
