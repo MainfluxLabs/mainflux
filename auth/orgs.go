4fba9c6f36b29e2c71df946e7e9ba0cea551a084
Zoran Rebic <56149596+zzokki81@users.noreply.github.com>
2023-10-16 17:09:36 +0200

    MF-330 - Fix comment
    Signed-off-by: Zoran Rebic <56149596+zzokki81@users.noreply.github.com>

--- auth/orgs.go
+++ auth/orgs.go
@@ -179,7 +179,7 @@ type Orgs interface {
 	// ListOrgGroups retrieves groups assigned to an org identified by orgID.
 	ListOrgGroups(ctx context.Context, token, orgID string, pm PageMetadata) (GroupsPage, error)
 
-	// RemovePolicies removes group policies for members.
+	// RemovePolicies removes group members policies.
 	RemovePolicies(ctx context.Context, token, orgID, groupID string, memberIDs ...string) error
 
 	// Backup retrieves all orgs, org relations and group relations. Only accessible by admin.
