40aa933fcf1df44e6394e5df71a77e3ef6db04e7
majabirmancevic <majabi2001@gmail.com>
2024-03-06 18:35:28 +0100

    Modified file request.go, added fields validation

--- webhooks/api/webhooks/http/requests.go
+++ webhooks/api/webhooks/http/requests.go
@@ -3,7 +3,17 @@
 
 package http
 
-import "github.com/MainfluxLabs/mainflux/webhooks"
+import (
+	"github.com/MainfluxLabs/mainflux/internal/apiutil"
+	"github.com/MainfluxLabs/mainflux/pkg/errors"
+	"net/url"
+)
+
+const (
+	maxNameSize = 1024
+	formatJSON  = "JSON"
+	formatSenML = "SenML"
+)
 
 type apiReq interface {
 	validate() error
@@ -17,8 +27,23 @@ type webhookReq struct {
 }
 
 func (req webhookReq) validate() error {
-	if req.name == "" {
-		return webhooks.ErrMalformedEntity
+	if req.name == "" || len(req.name) > maxNameSize {
+		return errors.New("missing or invalid name ")
+	}
+
+	if req.format == "" {
+		return errors.New("missing type of format")
+	} else if req.format != formatJSON || req.format != formatSenML {
+		return errors.New("invalid type of format")
+	}
+
+	if req.token == "" {
+		return apiutil.ErrBearerToken
+	}
+
+	_, err := url.ParseRequestURI(req.url)
+	if req.url == "" || err != nil {
+		return errors.New("missing or invalid url")
 	}
 
 	return nil
