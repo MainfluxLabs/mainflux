e5dfcaaf94bdcb2d196c8b53f15632556889619e
majabirmancevic <majabi2001@gmail.com>
2024-03-06 18:32:31 +0100

    Modified file logging.go

--- webhooks/api/logging.go
+++ webhooks/api/logging.go
@@ -26,7 +26,7 @@ func LoggingMiddleware(svc webhooks.Service, logger log.Logger) webhooks.Service
 	return &loggingMiddleware{logger, svc}
 }
 
-func (lm *loggingMiddleware) Ping(secret string) (response string, err error) {
+func (lm *loggingMiddleware) CreateWebhook(secret string) (response bool, err error) {
 	defer func(begin time.Time) {
 		message := fmt.Sprintf("Method ping for secret %s took %s to complete", secret, time.Since(begin))
 		if err != nil {
@@ -36,5 +36,5 @@ func (lm *loggingMiddleware) Ping(secret string) (response string, err error) {
 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
 	}(time.Now())
 
-	return lm.svc.Ping(secret)
+	return lm.svc.CreateWebhook(secret)
 }
