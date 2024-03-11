391fbbaeb30b98948d498edb220dfe7295d854b3
majabirmancevic <majabi2001@gmail.com>
2024-03-11 12:28:10 +0100

    Modified file endpoint.go

--- webhooks/api/webhooks/http/endpoint.go
+++ webhooks/api/webhooks/http/endpoint.go
@@ -15,16 +15,21 @@ func createWebhookEndpoint(svc webhooks.Service) endpoint.Endpoint {
 		req := request.(webhookReq)
 
 		if err := req.validate(); err != nil {
-			return nil, err
+			return webhookRes{created: false}, err
 		}
 
-		response, err := svc.CreateWebhook(req.name)
+		wh := webhooks.Webhook{
+			Name:   req.name,
+			Format: req.format,
+			Url:    req.url,
+		}
+		_, err := svc.CreateWebhook(ctx, req.token, wh)
 		if err != nil {
 			return nil, err
 		}
 
 		res := webhookRes{
-			created: response,
+			created: true,
 		}
 		return res, nil
 	}
