2cf25aad704deda481e705c35bbda8a071a78bdf
majabirmancevic <majabi2001@gmail.com>
2024-03-13 22:51:32 +0100

    Changed structure of Webhook

--- webhooks/webhooks.go
+++ webhooks/webhooks.go
@@ -3,15 +3,15 @@ package webhooks
 import "context"
 
 type Webhook struct {
-	ID     string
-	Name   string
-	Format string
-	Url    string
+	ThingID string
+	Name    string
+	Format  string
+	Url     string
 }
 
 type WebhookRepository interface {
 	// Save persists webhook. A non-nil error is returned to indicate
 	// operation failure.
 	Save(ctx context.Context, w Webhook) (Webhook, error)
-	RetrieveAll(ctx context.Context) ([]Webhook, error)
+	RetrieveByThingID(ctx context.Context, thingID string) ([]Webhook, error)
 }
