b11de962893c6286010b7cd5bdf40cf50d6208d0
Maja Birmančević <79976249+majabirmancevic@users.noreply.github.com>
2024-07-17 13:48:35 +0200

    NOISSUE - Move isEmail function from users to internal package (#487)

--- users/users.go
+++ users/users.go
@@ -5,27 +5,9 @@ package users
 
 import (
 	"context"
-	"fmt"
-	"regexp"
-	"strings"
 
+	"github.com/MainfluxLabs/mainflux/internal/email"
 	"github.com/MainfluxLabs/mainflux/pkg/errors"
-	"golang.org/x/net/idna"
-)
-
-const (
-	maxLocalLen  = 64
-	maxDomainLen = 255
-	maxTLDLen    = 24 // longest TLD currently in existence
-
-	atSeparator  = "@"
-	dotSeparator = "."
-)
-
-var (
-	userRegexp    = regexp.MustCompile("^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+$")
-	hostRegexp    = regexp.MustCompile(`^[^\s]+\.[^\s]+$`)
-	userDotRegexp = regexp.MustCompile("(^[.]{1})|([.]{1}$)|([.]{2,})")
 )
 
 // Metadata to be used for Mainflux thing or channel for customized
@@ -45,7 +27,7 @@ type User struct {
 
 // Validate returns an error if user representation is invalid.
 func (u User) Validate() error {
-	if !isEmail(u.Email) {
+	if !email.IsEmail(u.Email) {
 		return errors.ErrMalformedEntity
 	}
 
@@ -82,65 +64,4 @@ type UserRepository interface {
 
 	// RetrieveAll retrieves all users.
 	RetrieveAll(ctx context.Context) ([]User, error)
-}
-
-func isEmail(email string) bool {
-	if email == "" {
-		return false
-	}
-
-	es := strings.Split(email, atSeparator)
-	if len(es) != 2 {
-		return false
-	}
-	local, host := es[0], es[1]
-
-	if local == "" || len(local) > maxLocalLen {
-		return false
-	}
-
-	hs := strings.Split(host, dotSeparator)
-	if len(hs) < 2 {
-		return false
-	}
-	domain, ext := hs[0], hs[1]
-
-	// Check subdomain and validate
-	if len(hs) > 2 {
-		if domain == "" {
-			return false
-		}
-
-		for i := 1; i < len(hs)-1; i++ {
-			sub := hs[i]
-			if sub == "" {
-				return false
-			}
-			domain = fmt.Sprintf("%s.%s", domain, sub)
-		}
-
-		ext = hs[len(hs)-1]
-	}
-
-	if domain == "" || len(domain) > maxDomainLen {
-		return false
-	}
-	if ext == "" || len(ext) > maxTLDLen {
-		return false
-	}
-
-	punyLocal, err := idna.ToASCII(local)
-	if err != nil {
-		return false
-	}
-	punyHost, err := idna.ToASCII(host)
-	if err != nil {
-		return false
-	}
-
-	if userDotRegexp.MatchString(punyLocal) || !userRegexp.MatchString(punyLocal) || !hostRegexp.MatchString(punyHost) {
-		return false
-	}
-
-	return true
 }
