a59adcc28474ff3171b8f4c6cba1f3bb00441d64
Manuel Imperiale <manuel.imperiale@gmail.com>
2024-09-18 12:19:29 +0200

    NOISSUE - Fix nginx entrypoint
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -23,6 +23,7 @@ envsubst '
     ${MF_POSTGRES_READER_PORT}
     ${MF_WEBHOOKS_HTTP_PORT}
     ${MF_SMTP_NOTIFIER_PORT}
+    ${MF_DOWNLINKS_HTTP_PORT}
     ${MF_FILESTORE_HTTP_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
