7d606876f0f089678906462e9fd8300704bb3ff2
duolok <dusan.lecic5@gmail.com>
2025-08-28 14:27:29 +0200

    update: new logging and metrics

--- readers/api/logging.go
+++ readers/api/logging.go
@@ -29,9 +29,61 @@ func LoggingMiddleware(svc readers.MessageRepository, logger logger.Logger) read
 	}
 }
 
-func (lm *loggingMiddleware) ListAllMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+// func (lm *loggingMiddleware) ListAllMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+// 	defer func(begin time.Time) {
+// 		message := fmt.Sprintf("Method list_all_messages took %s to complete", time.Since(begin))
+// 		if err != nil {
+// 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+// 			return
+// 		}
+// 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+// 	}(time.Now())
+//
+// 	return lm.svc.ListAllMessages(rpm)
+// }
+//
+// func (lm *loggingMiddleware) DeleteMessages(ctx context.Context, rpm readers.PageMetadata) (err error) {
+// 	defer func(begin time.Time) {
+// 		message := fmt.Sprintf("Method delete_messages took %s to complete", time.Since(begin))
+// 		if err != nil {
+// 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+// 			return
+// 		}
+// 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+// 	}(time.Now())
+//
+// 	return lm.svc.DeleteMessages(ctx, rpm)
+// }
+//
+// func (lm *loggingMiddleware) Backup(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+// 	defer func(begin time.Time) {
+// 		message := fmt.Sprintf("Method backup took %s to complete", time.Since(begin))
+// 		if err != nil {
+// 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+// 			return
+// 		}
+// 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+// 	}(time.Now())
+//
+// 	return lm.svc.Backup(rpm)
+// }
+//
+// func (lm *loggingMiddleware) Restore(ctx context.Context, format string, messages ...readers.Message) (err error) {
+// 	defer func(begin time.Time) {
+// 		message := fmt.Sprintf("Method restore took %s to complete", time.Since(begin))
+// 		if err != nil {
+// 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+// 			return
+// 		}
+// 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+// 	}(time.Now())
+//
+// 	return lm.svc.Restore(ctx, format, messages...)
+// }
+
+func (lm *loggingMiddleware) ListJSONMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
 	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method list_all_messages took %s to complete", time.Since(begin))
+		message := fmt.Sprintf("Method list_json_messages took %s to complete", time.Since(begin))
 		if err != nil {
 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
 			return
@@ -39,10 +91,88 @@ func (lm *loggingMiddleware) ListAllMessages(rpm readers.PageMetadata) (page rea
 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
 	}(time.Now())
 
-	return lm.svc.ListAllMessages(rpm)
+	return lm.svc.ListJSONMessages(rpm)
 }
 
-func (lm *loggingMiddleware) DeleteMessages(ctx context.Context, rpm readers.PageMetadata) (err error) {
+func (lm *loggingMiddleware) ListSenMLMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method list_senml_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.ListSenMLMessages(rpm)
+}
+
+func (lm *loggingMiddleware) BackupJSONMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method backup_json_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.BackupJSONMessages(rpm)
+}
+
+func (lm *loggingMiddleware) BackupSenMLMessages(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method backup_senml_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.BackupSenMLMessages(rpm)
+}
+
+func (lm *loggingMiddleware) RestoreJSONMessages(ctx context.Context, messages ...readers.Message) (err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method restore_json_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.RestoreSenMLMessageS(ctx, messages...)
+}
+
+func (lm *loggingMiddleware) RestoreSenMLMessageS(ctx context.Context, messages ...readers.Message) (err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method restore_senml_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.RestoreSenMLMessageS(ctx, messages...)
+}
+
+func (lm *loggingMiddleware) DeleteJSONMessages(ctx context.Context, rpm readers.PageMetadata) (err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method delete_json_messages took %s to complete", time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.DeleteJSONMessages(ctx, rpm)
+}
+
+func (lm *loggingMiddleware) DeleteSenMLMessages(ctx context.Context, rpm readers.PageMetadata) (err error) {
 	defer func(begin time.Time) {
 		message := fmt.Sprintf("Method delete_messages took %s to complete", time.Since(begin))
 		if err != nil {
@@ -52,31 +182,5 @@ func (lm *loggingMiddleware) DeleteMessages(ctx context.Context, rpm readers.Pag
 		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
 	}(time.Now())
 
-	return lm.svc.DeleteMessages(ctx, rpm)
-}
-
-func (lm *loggingMiddleware) Backup(rpm readers.PageMetadata) (page readers.MessagesPage, err error) {
-	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method backup took %s to complete", time.Since(begin))
-		if err != nil {
-			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
-			return
-		}
-		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
-	}(time.Now())
-
-	return lm.svc.Backup(rpm)
-}
-
-func (lm *loggingMiddleware) Restore(ctx context.Context, format string, messages ...readers.Message) (err error) {
-	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method restore took %s to complete", time.Since(begin))
-		if err != nil {
-			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
-			return
-		}
-		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
-	}(time.Now())
-
-	return lm.svc.Restore(ctx, format, messages...)
+	return lm.svc.DeleteSenMLMessages(ctx, rpm)
 }
--- readers/api/metrics.go
+++ readers/api/metrics.go
@@ -30,38 +30,110 @@ func MetricsMiddleware(svc readers.MessageRepository, counter metrics.Counter, l
 	}
 }
 
-func (mm *metricsMiddleware) ListAllMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
+//	func (mm *metricsMiddleware) ListAllMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
+//		defer func(begin time.Time) {
+//			mm.counter.With("method", "list_all_messages").Add(1)
+//			mm.latency.With("method", "list_all_messages").Observe(time.Since(begin).Seconds())
+//		}(time.Now())
+//
+//		return mm.svc.ListAllMessages(rpm)
+//	}
+//
+//	func (mm *metricsMiddleware) DeleteMessages(ctx context.Context, rpm readers.PageMetadata) error {
+//		defer func(begin time.Time) {
+//			mm.counter.With("method", "delete_messages").Add(1)
+//			mm.latency.With("method", "delete_messages").Observe(time.Since(begin).Seconds())
+//		}(time.Now())
+//
+//		return mm.svc.DeleteMessages(ctx, rpm)
+//	}
+//
+//	func (mm *metricsMiddleware) Backup(rpm readers.PageMetadata) (readers.MessagesPage, error) {
+//		defer func(begin time.Time) {
+//			mm.counter.With("method", "backup").Add(1)
+//			mm.latency.With("method", "backup").Observe(time.Since(begin).Seconds())
+//		}(time.Now())
+//
+//		return mm.svc.Backup(rpm)
+//	}
+//
+//	func (mm *metricsMiddleware) Restore(ctx context.Context, format string, messages ...readers.Message) error {
+//		defer func(begin time.Time) {
+//			mm.counter.With("method", "restore").Add(1)
+//			mm.latency.With("method", "restore").Observe(time.Since(begin).Seconds())
+//		}(time.Now())
+//
+//		return mm.svc.Restore(ctx, format, messages...)
+//	}
+func (mm *metricsMiddleware) ListJSONMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
 	defer func(begin time.Time) {
-		mm.counter.With("method", "list_all_messages").Add(1)
-		mm.latency.With("method", "list_all_messages").Observe(time.Since(begin).Seconds())
+		mm.counter.With("method", "list_json_messages").Add(1)
+		mm.latency.With("method", "list_json_messages").Observe(time.Since(begin).Seconds())
 	}(time.Now())
 
-	return mm.svc.ListAllMessages(rpm)
+	return mm.svc.ListJSONMessages(rpm)
 }
 
-func (mm *metricsMiddleware) DeleteMessages(ctx context.Context, rpm readers.PageMetadata) error {
+func (mm *metricsMiddleware) ListSenMLMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
 	defer func(begin time.Time) {
-		mm.counter.With("method", "delete_messages").Add(1)
-		mm.latency.With("method", "delete_messages").Observe(time.Since(begin).Seconds())
+		mm.counter.With("method", "list_senml_messages").Add(1)
+		mm.latency.With("method", "list_senml_messages").Observe(time.Since(begin).Seconds())
 	}(time.Now())
 
-	return mm.svc.DeleteMessages(ctx, rpm)
+	return mm.svc.ListSenMLMessages(rpm)
 }
 
-func (mm *metricsMiddleware) Backup(rpm readers.PageMetadata) (readers.MessagesPage, error) {
+func (mm *metricsMiddleware) BackupJSONMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
 	defer func(begin time.Time) {
-		mm.counter.With("method", "backup").Add(1)
-		mm.latency.With("method", "backup").Observe(time.Since(begin).Seconds())
+		mm.counter.With("method", "backup_json_messages").Add(1)
+		mm.latency.With("method", "backup_json_messages").Observe(time.Since(begin).Seconds())
 	}(time.Now())
 
-	return mm.svc.Backup(rpm)
+	return mm.svc.BackupJSONMessages(rpm)
+
 }
 
-func (mm *metricsMiddleware) Restore(ctx context.Context, format string, messages ...readers.Message) error {
+func (mm *metricsMiddleware) BackupSenMLMessages(rpm readers.PageMetadata) (readers.MessagesPage, error) {
 	defer func(begin time.Time) {
-		mm.counter.With("method", "restore").Add(1)
-		mm.latency.With("method", "restore").Observe(time.Since(begin).Seconds())
+		mm.counter.With("method", "backup_senml_messages").Add(1)
+		mm.latency.With("method", "backup_senml_messages").Observe(time.Since(begin).Seconds())
 	}(time.Now())
 
-	return mm.svc.Restore(ctx, format, messages...)
+	return mm.svc.BackupSenMLMessages(rpm)
+}
+
+func (mm *metricsMiddleware) RestoreJSONMessages(ctx context.Context, messages ...readers.Message) error {
+	defer func(begin time.Time) {
+		mm.counter.With("method", "restore_json_messages").Add(1)
+		mm.latency.With("method", "restore_json_messages").Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return mm.svc.RestoreJSONMessages(ctx, messages...)
+}
+
+func (mm *metricsMiddleware) RestoreSenMLMessageS(ctx context.Context, messages ...readers.Message) error {
+	defer func(begin time.Time) {
+		mm.counter.With("method", "restore_senml_messages").Add(1)
+		mm.latency.With("method", "restore_senml_messages").Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return mm.svc.RestoreSenMLMessageS(ctx, messages...)
+}
+
+func (mm *metricsMiddleware) DeleteJSONMessages(ctx context.Context, rpm readers.PageMetadata) error {
+	defer func(begin time.Time) {
+		mm.counter.With("method", "delete_json_messages").Add(1)
+		mm.latency.With("method", "delete_json_messages").Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return mm.svc.DeleteJSONMessages(ctx, rpm)
+}
+
+func (mm *metricsMiddleware) DeleteSenMLMessages(ctx context.Context, rpm readers.PageMetadata) error {
+	defer func(begin time.Time) {
+		mm.counter.With("method", "delete_json_messages").Add(1)
+		mm.latency.With("method", "delete_json_messages").Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return mm.svc.DeleteSenMLMessages(ctx, rpm)
 }
