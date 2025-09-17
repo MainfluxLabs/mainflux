256882f49b737b805458c0789c3795d37a7f26f5
duolok <dusan.lecic5@gmail.com>
2025-09-17 16:52:52 +0200

    init: readers service
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ readers/service.go
@@ -0,0 +1,75 @@
+package readers
+
+import "context"
+
+// Service specifies an API that must be fullfiled by the domain service
+// implementation, and all of its decorators (e.g. logging & metrics).
+type Service interface {
+	// ListJSONMessages retrieves the json messages with given filters.
+	ListJSONMessages(ctx context.Context, rpm JSONPageMetadata) (JSONMessagesPage, error)
+
+	// ListSenMLMessages retrieves the senml messages with given filters.
+	ListSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) (SenMLMessagesPage, error)
+
+	// BackupJSONMessages backups the json messages with given filters.
+	BackupJSONMessages(ctx context.Context, rpm JSONPageMetadata) (JSONMessagesPage, error)
+
+	// BackupSenMLMessages backups the senml messages with given filters.
+	BackupSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) (SenMLMessagesPage, error)
+
+	// RestoreJSONMessages restores the json messages.
+	RestoreJSONMessages(ctx context.Context, messages ...Message) error
+
+	// RestoreSenMLMessages restores the senml messages.
+	RestoreSenMLMessages(ctx context.Context, messages ...Message) error
+
+	// DeleteJSONMessages deletes the json messages within a time range.
+	DeleteJSONMessages(ctx context.Context, rpm JSONPageMetadata) error
+
+	// DeleteSenMLMessages deletes the json messages within a time range.
+	DeleteSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) error
+}
+
+type readersService struct {
+	json  JSONMessaageRepository
+	senml SenMLMessageRepository
+}
+
+func New(json JSONMessaageRepository, senml SenMLMessageRepository) Service {
+	return &readersService{
+		json:  json,
+		senml: senml,
+	}
+}
+
+func (rs *readersService) ListJSONMessages(ctx context.Context, rpm JSONPageMetadata) (JSONMessagesPage, error) {
+	return rs.json.ListMessages(ctx, rpm)
+}
+
+func (rs *readersService) ListSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) (SenMLMessagesPage, error) {
+	return rs.senml.ListMessages(ctx, rpm)
+}
+
+func (rs *readersService) BackupJSONMessages(ctx context.Context, rpm JSONPageMetadata) (JSONMessagesPage, error) {
+	return rs.json.Backup(ctx, rpm)
+}
+
+func (rs *readersService) BackupSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) (SenMLMessagesPage, error) {
+	return rs.senml.Backup(ctx, rpm)
+}
+
+func (rs *readersService) RestoreJSONMessages(ctx context.Context, messages ...Message) error {
+	return rs.json.Restore(ctx, messages...)
+}
+
+func (rs *readersService) RestoreSenMLMessages(ctx context.Context, messages ...Message) error {
+	return rs.senml.Restore(ctx, messages...)
+}
+
+func (rs *readersService) DeleteJSONMessages(ctx context.Context, rpm JSONPageMetadata) error {
+	return rs.json.Delete(ctx, rpm)
+}
+
+func (rs *readersService) DeleteSenMLMessages(ctx context.Context, rpm SenMLPageMetadata) error {
+	return rs.senml.Delete(ctx, rpm)
+}
