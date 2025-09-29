ce67c117cd5b2b659e6f4b0852f9b22cf6bb35b1
Maja Birmančević <79976249+majabirmancevic@users.noreply.github.com>
2025-09-29 20:23:09 +0200

    MF-849 - Publish all messages via rules (#850)
    * MF-849 - Remove unused pkg helpers and consts
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-849 - Publish all messages via rules
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-849 - Remove publisher dependency from http-adapter service
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-849 - Subscribe writers to SubjectWriters
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-849 - Use logger in publish method
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    ---------
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>

--- http/adapter.go
+++ http/adapter.go
@@ -7,11 +7,9 @@ package http
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/MainfluxLabs/mainflux/logger"
 	"github.com/MainfluxLabs/mainflux/pkg/messaging"
-	"github.com/MainfluxLabs/mainflux/pkg/messaging/nats"
 	protomfx "github.com/MainfluxLabs/mainflux/pkg/proto"
 )
 
@@ -24,19 +22,17 @@ type Service interface {
 var _ Service = (*adapterService)(nil)
 
 type adapterService struct {
-	publisher messaging.Publisher
-	things    protomfx.ThingsServiceClient
-	rules     protomfx.RulesServiceClient
-	logger    logger.Logger
+	things protomfx.ThingsServiceClient
+	rules  protomfx.RulesServiceClient
+	logger logger.Logger
 }
 
 // New instantiates the HTTP adapter implementation.
-func New(publisher messaging.Publisher, things protomfx.ThingsServiceClient, rules protomfx.RulesServiceClient, logger logger.Logger) Service {
+func New(things protomfx.ThingsServiceClient, rules protomfx.RulesServiceClient, logger logger.Logger) Service {
 	return &adapterService{
-		publisher: publisher,
-		things:    things,
-		rules:     rules,
-		logger:    logger,
+		things: things,
+		rules:  rules,
+		logger: logger,
 	}
 }
 
@@ -51,24 +47,8 @@ func (as *adapterService) Publish(ctx context.Context, key string, message proto
 		return err
 	}
 
-	msg := message
-	go func(m protomfx.Message) {
-		_, err := as.rules.Publish(context.Background(), &protomfx.PublishReq{Message: &m})
-		if err != nil {
-			as.logger.Error(fmt.Sprintf("%s: %s", messaging.ErrPublishMessage, err))
-		}
-	}(msg)
-
-	subjects := nats.GetSubjects(message.Subtopic)
-	for _, sub := range subjects {
-		msg := message
-		msg.Subject = sub
-
-		go func(m protomfx.Message) {
-			if err := as.publisher.Publish(m); err != nil {
-				as.logger.Error(fmt.Sprintf("%s: %s", messaging.ErrPublishMessage, err))
-			}
-		}(msg)
+	if _, err = as.rules.Publish(context.Background(), &protomfx.PublishReq{Message: &message}); err != nil {
+		return err
 	}
 
 	return nil
