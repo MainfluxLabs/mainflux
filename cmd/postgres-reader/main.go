41016c81ecd384d48ed401a63b9c9714ebf51d92
duolok <dusan.lecic5@gmail.com>
2025-10-03 13:15:35 +0200

    refactor: correct postgres tracer naming
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- cmd/postgres-reader/main.go
+++ cmd/postgres-reader/main.go
@@ -120,7 +120,7 @@ func main() {
 	db := connectToDB(cfg.dbConfig, logger)
 	defer db.Close()
 
-	dbTracer, dbCloser := jaeger.Init("messages_postgres_db", cfg.jaegerURL, logger)
+	dbTracer, dbCloser := jaeger.Init("postgres_db", cfg.jaegerURL, logger)
 	defer dbCloser.Close()
 
 	svc := newService(db, dbTracer, auth, tc, logger)
