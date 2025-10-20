a5bacb789edc1f7fba9cb1ad936da8cb6d43d5f5
Maja Birmancevic <majabi2001@gmail.com>
2025-10-20 17:37:48 +0200

    MF-894 - Fix UpdateRule
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>

--- rules/service.go
+++ rules/service.go
@@ -130,7 +130,13 @@ func (rs *rulesService) ViewRule(ctx context.Context, token, id string) (Rule, e
 }
 
 func (rs *rulesService) UpdateRule(ctx context.Context, token string, rule Rule) error {
-	if _, err := rs.things.CanUserAccessGroup(ctx, &protomfx.UserAccessReq{Token: token, Id: rule.GroupID, Action: things.Editor}); err != nil {
+	r, err := rs.rules.RetrieveByID(ctx, rule.ID)
+	if err != nil {
+		return err
+	}
+
+	req := &protomfx.UserAccessReq{Token: token, Id: r.GroupID, Action: things.Editor}
+	if _, err := rs.things.CanUserAccessGroup(ctx, req); err != nil {
 		return err
 	}
 
