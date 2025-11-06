84249dae0277b3862f4fc17f92bbb6f5119d5212
duolok <dusan.lecic5@gmail.com>
2025-11-06 18:02:04 +0100

    update: mocks
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- certs/mocks/certs.go
+++ certs/mocks/certs.go
@@ -1,6 +1,5 @@
 // Copyright (c) Mainflux
 // SPDX-License-Identifier: Apache-2.0
-
 package mocks
 
 import (
@@ -18,14 +17,14 @@ type certsRepoMock struct {
 	mu             sync.Mutex
 	counter        uint64
 	certsBySerial  map[string]certs.Cert
-	certsByThingID map[string]map[string][]certs.Cert
+	certsByThingID map[string][]certs.Cert
 	revokedCerts   map[string]certs.RevokedCert
 }
 
 func NewCertsRepository() certs.Repository {
 	return &certsRepoMock{
 		certsBySerial:  make(map[string]certs.Cert),
-		certsByThingID: make(map[string]map[string][]certs.Cert),
+		certsByThingID: make(map[string][]certs.Cert),
 		revokedCerts:   make(map[string]certs.RevokedCert),
 	}
 }
@@ -35,7 +34,6 @@ func (c *certsRepoMock) Save(ctx context.Context, cert certs.Cert) (string, erro
 	defer c.mu.Unlock()
 
 	crt := certs.Cert{
-		OwnerID:        cert.OwnerID,
 		ThingID:        cert.ThingID,
 		Serial:         cert.Serial,
 		Expire:         cert.Expire,
@@ -46,22 +44,18 @@ func (c *certsRepoMock) Save(ctx context.Context, cert certs.Cert) (string, erro
 		PrivateKeyType: cert.PrivateKeyType,
 	}
 
-	if _, ok := c.certsByThingID[cert.OwnerID]; !ok {
-		c.certsByThingID[cert.OwnerID] = make(map[string][]certs.Cert)
+	if _, ok := c.certsByThingID[cert.ThingID]; !ok {
+		c.certsByThingID[cert.ThingID] = []certs.Cert{}
 	}
 
-	if _, ok := c.certsByThingID[cert.OwnerID][cert.ThingID]; !ok {
-		c.certsByThingID[cert.OwnerID][cert.ThingID] = []certs.Cert{}
-	}
-
-	c.certsByThingID[cert.OwnerID][cert.ThingID] = append(c.certsByThingID[cert.OwnerID][cert.ThingID], crt)
-
+	c.certsByThingID[cert.ThingID] = append(c.certsByThingID[cert.ThingID], crt)
 	c.certsBySerial[cert.Serial] = crt
 	c.counter++
+
 	return cert.Serial, nil
 }
 
-func (c *certsRepoMock) RetrieveAll(ctx context.Context, ownerID string, offset, limit uint64) (certs.Page, error) {
+func (c *certsRepoMock) RetrieveAll(ctx context.Context, offset, limit uint64) (certs.Page, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -69,13 +63,9 @@ func (c *certsRepoMock) RetrieveAll(ctx context.Context, ownerID string, offset,
 		return certs.Page{}, nil
 	}
 
-	ownerCerts, ok := c.certsByThingID[ownerID]
-	if !ok {
-		return certs.Page{}, dbutil.ErrNotFound
-	}
-
+	// Collect all certificates from all things
 	var allCerts []certs.Cert
-	for _, thingCerts := range ownerCerts {
+	for _, thingCerts := range c.certsByThingID {
 		allCerts = append(allCerts, thingCerts...)
 	}
 
@@ -83,6 +73,7 @@ func (c *certsRepoMock) RetrieveAll(ctx context.Context, ownerID string, offset,
 	if start > uint64(len(allCerts)) {
 		start = uint64(len(allCerts))
 	}
+
 	end := start + limit
 	if end > uint64(len(allCerts)) {
 		end = uint64(len(allCerts))
@@ -92,54 +83,36 @@ func (c *certsRepoMock) RetrieveAll(ctx context.Context, ownerID string, offset,
 		Certs: allCerts[start:end],
 		Total: uint64(len(allCerts)),
 	}
+
 	return page, nil
 }
 
-func (c *certsRepoMock) Remove(ctx context.Context, ownerID, serialID string) error {
+func (c *certsRepoMock) Remove(ctx context.Context, thingID string) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	cert, ok := c.certsBySerial[serialID]
+	thingCerts, ok := c.certsByThingID[thingID]
 	if !ok {
 		return dbutil.ErrNotFound
 	}
 
-	if cert.OwnerID != ownerID {
-		return dbutil.ErrNotFound
-	}
+	// Remove all certificates for this thing
+	for _, cert := range thingCerts {
+		delete(c.certsBySerial, cert.Serial)
 
-	thingID := cert.ThingID
-
-	delete(c.certsBySerial, serialID)
-
-	c.revokedCerts[serialID] = certs.RevokedCert{
-		Serial:    serialID,
-		OwnerID:   ownerID,
-		ThingID:   thingID,
-		RevokedAt: time.Now(),
-	}
-
-	if thingCerts, ok := c.certsByThingID[ownerID][thingID]; ok {
-		for i, tc := range thingCerts {
-			if tc.Serial == serialID {
-				c.certsByThingID[ownerID][thingID] = append(thingCerts[:i], thingCerts[i+1:]...)
-				break
-			}
-		}
-
-		if len(c.certsByThingID[ownerID][thingID]) == 0 {
-			delete(c.certsByThingID[ownerID], thingID)
-		}
-
-		if len(c.certsByThingID[ownerID]) == 0 {
-			delete(c.certsByThingID, ownerID)
+		c.revokedCerts[cert.Serial] = certs.RevokedCert{
+			Serial:    cert.Serial,
+			ThingID:   thingID,
+			RevokedAt: time.Now(),
 		}
 	}
+
+	delete(c.certsByThingID, thingID)
 
 	return nil
 }
 
-func (c *certsRepoMock) RetrieveByThing(ctx context.Context, ownerID, thingID string, offset, limit uint64) (certs.Page, error) {
+func (c *certsRepoMock) RetrieveByThing(ctx context.Context, thingID string, offset, limit uint64) (certs.Page, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -147,7 +120,7 @@ func (c *certsRepoMock) RetrieveByThing(ctx context.Context, ownerID, thingID st
 		return certs.Page{}, nil
 	}
 
-	thingCerts, ok := c.certsByThingID[ownerID][thingID]
+	thingCerts, ok := c.certsByThingID[thingID]
 	if !ok {
 		return certs.Page{}, dbutil.ErrNotFound
 	}
@@ -156,6 +129,7 @@ func (c *certsRepoMock) RetrieveByThing(ctx context.Context, ownerID, thingID st
 	if start > uint64(len(thingCerts)) {
 		start = uint64(len(thingCerts))
 	}
+
 	end := start + limit
 	if end > uint64(len(thingCerts)) {
 		end = uint64(len(thingCerts))
@@ -165,19 +139,16 @@ func (c *certsRepoMock) RetrieveByThing(ctx context.Context, ownerID, thingID st
 		Certs: thingCerts[start:end],
 		Total: uint64(len(thingCerts)),
 	}
+
 	return page, nil
 }
 
-func (c *certsRepoMock) RetrieveBySerial(ctx context.Context, ownerID, serialID string) (certs.Cert, error) {
+func (c *certsRepoMock) RetrieveBySerial(ctx context.Context, serialID string) (certs.Cert, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	crt, ok := c.certsBySerial[serialID]
 	if !ok {
-		return certs.Cert{}, dbutil.ErrNotFound
-	}
-
-	if crt.OwnerID != ownerID {
 		return certs.Cert{}, dbutil.ErrNotFound
 	}
 
