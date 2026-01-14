93ae4580de2d92184ec7a2cfc4519de63818d3bc
Maja Birmancevic <majabi2001@gmail.com>
2026-01-14 15:20:48 +0100

    MF-988 - Remove Redis events
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>

--- cmd/mqtt/main.go
+++ cmd/mqtt/main.go
@@ -16,7 +16,6 @@ import (
 	mqttapi "github.com/MainfluxLabs/mainflux/mqtt/api"
 	mqttapihttp "github.com/MainfluxLabs/mainflux/mqtt/api/http"
 	"github.com/MainfluxLabs/mainflux/mqtt/postgres"
-	mqttredis "github.com/MainfluxLabs/mainflux/mqtt/redis"
 	"github.com/MainfluxLabs/mainflux/pkg/clients"
 	clientsgrpc "github.com/MainfluxLabs/mainflux/pkg/clients/grpc"
 	"github.com/MainfluxLabs/mainflux/pkg/errors"
@@ -213,8 +212,6 @@ func main() {
 	}
 	defer np.Close()
 
-	es := mqttredis.NewEventStore(ec, cfg.instance)
-
 	ac := connectToRedis(cfg.authCacheURL, cfg.authPass, cfg.authCacheDB, logger)
 	defer ac.Close()
 
@@ -237,7 +234,7 @@ func main() {
 	svc := newService(usersAuth, tc, db, logger)
 
 	// Event handler for MQTT hooks
-	h := mqtt.NewHandler(np, es, logger, tc, svc)
+	h := mqtt.NewHandler(np, logger, tc, svc)
 
 	logger.Info(fmt.Sprintf("Starting MQTT proxy on port %s", cfg.port))
 	g.Go(func() error {
