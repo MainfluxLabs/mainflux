833fcfdc37ca637d77ecf524321eb8bc1ec12a9e
Maja Birmančević <79976249+majabirmancevic@users.noreply.github.com>
2026-01-26 10:14:28 +0100

    MF-988 - Refactor MQTT subscriptions to be thing-centric (#989)
    * MF-988 - Remove status from Postgres
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Remove Redis events
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Remove client_id
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Add redis cache
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Add mock
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Remove event store envs
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Improve mqtt_3 migration
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Fix logs
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Rename GetThingByClient to RetrieveThingByClient
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    * MF-988 - Rename thingByClientKey to thingByClientIDKey
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>
    
    ---------
    
    Signed-off-by: Maja Birmancevic <majabi2001@gmail.com>

--- mqtt/api/logging.go
+++ mqtt/api/logging.go
@@ -67,30 +67,3 @@ func (lm *loggingMiddleware) RemoveSubscription(ctx context.Context, sub mqtt.Su
 
 	return lm.svc.RemoveSubscription(ctx, sub)
 }
-
-func (lm *loggingMiddleware) HasClientID(ctx context.Context, clientID string) (err error) {
-	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method has_client_id for client id %s took %s to complete", clientID, time.Since(begin))
-		if err != nil {
-			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
-			return
-		}
-		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
-	}(time.Now())
-
-	return lm.svc.HasClientID(ctx, clientID)
-}
-
-func (lm *loggingMiddleware) UpdateStatus(ctx context.Context, sub mqtt.Subscription) (err error) {
-	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method update_status for thing id %s, group id %s and subtopic %s took %s to complete",
-			sub.ThingID, sub.GroupID, sub.Subtopic, time.Since(begin))
-		if err != nil {
-			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
-			return
-		}
-		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
-	}(time.Now())
-
-	return lm.svc.UpdateStatus(ctx, sub)
-}
