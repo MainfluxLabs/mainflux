35c51ddbb0da59d01081f3fa81a1acac91554bc7
Dusan Lecic <42711489+duolok@users.noreply.github.com>
2026-02-13 09:00:54 -0800

    MF-1003 - Multi-device search for readers (#1027)
    * add: search endpoint
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * add: search request
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * add: search response
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * add: decode func and define endpoint
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remove: bad mutex usage
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * refactor: separate senml and json logic for getting messages
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remove: wrappers around structs to make it more clean
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * update: openapi readers specification
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * refactor: multi status response
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * fix: correct response status and openapi specification
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * refactor: reduce duplication in request validation
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remove: thing key usage from searches
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remove: unnecessary body checks
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * refactor: remove pointer check
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remove: thingAuth from serach endpoints in openapi
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * add: goroutine concurrency limit
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * add: concurrency limit const
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * rename: MaxConcurrency to ConcurrencyLimit
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * fix: empty line pr ramark
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * refactor: rename Searches to jsonPageMetadatas/senmlPageMetadatas
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    * remark: add blank lines in decode func
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>
    
    ---------
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- readers/api/http/messages/responses.go
+++ readers/api/http/messages/responses.go
@@ -80,3 +80,71 @@ func (res removeRes) Headers() map[string]string {
 func (res removeRes) Empty() bool {
 	return true
 }
+
+type searchJSONMessagesRes []searchJSONResultItem
+
+type searchJSONResultItem struct {
+	Total    uint64            `json:"total"`
+	Messages []readers.Message `json:"messages"`
+	Error    string            `json:"error,omitempty"`
+}
+
+func (res searchJSONMessagesRes) Headers() map[string]string {
+	return map[string]string{}
+}
+
+func (res searchJSONMessagesRes) Code() int {
+	errCount := 0
+	for _, r := range res {
+		if r.Error != "" {
+			errCount++
+		}
+	}
+
+	switch {
+	case errCount == 0:
+		return http.StatusOK
+	case errCount < len(res):
+		return http.StatusMultiStatus
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func (res searchJSONMessagesRes) Empty() bool {
+	return false
+}
+
+type searchSenMLMessagesRes []searchSenMLResultItem
+
+type searchSenMLResultItem struct {
+	Total    uint64            `json:"total"`
+	Messages []readers.Message `json:"messages"`
+	Error    string            `json:"error,omitempty"`
+}
+
+func (res searchSenMLMessagesRes) Headers() map[string]string {
+	return map[string]string{}
+}
+
+func (res searchSenMLMessagesRes) Code() int {
+	errCount := 0
+	for _, r := range res {
+		if r.Error != "" {
+			errCount++
+		}
+	}
+
+	switch {
+	case errCount == 0:
+		return http.StatusOK
+	case errCount < len(res):
+		return http.StatusMultiStatus
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func (res searchSenMLMessagesRes) Empty() bool {
+	return false
+}
