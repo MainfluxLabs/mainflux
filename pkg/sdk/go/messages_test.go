5c85d405f4a3d13399104477b66ead2a702350c6
duolok <dusan.lecic5@gmail.com>
2026-03-02 09:52:43 +0100

    add: new messages files
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ pkg/sdk/go/messages_test.go
@@ -0,0 +1,129 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package sdk_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	adapter "github.com/MainfluxLabs/mainflux/http"
+	"github.com/MainfluxLabs/mainflux/http/api"
+	"github.com/MainfluxLabs/mainflux/logger"
+	"github.com/MainfluxLabs/mainflux/pkg/mocks"
+	protomfx "github.com/MainfluxLabs/mainflux/pkg/proto"
+	sdk "github.com/MainfluxLabs/mainflux/pkg/sdk/go"
+	"github.com/MainfluxLabs/mainflux/things"
+	"github.com/opentracing/opentracing-go/mocktracer"
+	"github.com/stretchr/testify/assert"
+)
+
+func newMessageService(tc protomfx.ThingsServiceClient) adapter.Service {
+	pub := mocks.NewPublisher()
+	return adapter.New(pub, tc)
+}
+
+func newMessageServer(svc adapter.Service) *httptest.Server {
+	lm := logger.NewMock()
+	mux := api.MakeHandler(svc, mocktracer.New(), lm)
+	return httptest.NewServer(mux)
+}
+
+func TestSendMessage(t *testing.T) {
+	profileID := "1"
+	atoken := "auth_token"
+	invalidToken := "invalid"
+	msg := `[{"n":"current","t":-1,"v":1.6}]`
+	tc := mocks.NewThingsServiceClient(map[string]things.Profile{atoken: {ID: profileID}}, nil, nil)
+	pub := newMessageService(tc)
+	ts := newMessageServer(pub)
+	defer ts.Close()
+	sdkConf := sdk.Config{
+		HTTPAdapterURL:  ts.URL,
+		MsgContentType:  contentType,
+		TLSVerification: false,
+	}
+
+	mainfluxSDK := sdk.NewSDK(sdkConf)
+
+	cases := map[string]struct {
+		profileID string
+		msg       string
+		auth      string
+		err       error
+	}{
+		"publish message": {
+			profileID: profileID,
+			msg:       msg,
+			auth:      atoken,
+			err:       nil,
+		},
+		"publish message without authorization token": {
+			profileID: profileID,
+			msg:       msg,
+			auth:      "",
+			err:       createError(sdk.ErrFailedPublish, http.StatusUnauthorized),
+		},
+		"publish message with invalid authorization token": {
+			profileID: profileID,
+			msg:       msg,
+			auth:      invalidToken,
+			err:       createError(sdk.ErrFailedPublish, http.StatusUnauthorized),
+		},
+		"publish message with wrong content type": {
+			profileID: profileID,
+			msg:       "text",
+			auth:      atoken,
+			err:       nil,
+		},
+		"publish message unable to authorize": {
+			profileID: profileID,
+			msg:       msg,
+			auth:      invalidToken,
+			err:       createError(sdk.ErrFailedPublish, http.StatusUnauthorized),
+		},
+	}
+	for desc, tc := range cases {
+		err := mainfluxSDK.SendMessage(tc.profileID, tc.msg, things.ThingKey{Type: things.KeyTypeInternal, Value: tc.auth})
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", desc, tc.err, err))
+	}
+}
+
+func TestValidateContentType(t *testing.T) {
+	profileID := "1"
+	atoken := "auth_token"
+	tc := mocks.NewThingsServiceClient(map[string]things.Profile{atoken: {ID: profileID}}, nil, nil)
+	pub := newMessageService(tc)
+	ts := newMessageServer(pub)
+	defer ts.Close()
+
+	sdkConf := sdk.Config{
+		HTTPAdapterURL:  ts.URL,
+		MsgContentType:  contentType,
+		TLSVerification: false,
+	}
+	mainfluxSDK := sdk.NewSDK(sdkConf)
+
+	cases := []struct {
+		desc  string
+		cType sdk.ContentType
+		err   error
+	}{
+		{
+			desc:  "set senml+json content type",
+			cType: "application/senml+json",
+			err:   nil,
+		},
+		{
+			desc:  "set invalid content type",
+			cType: "invalid",
+			err:   sdk.ErrInvalidContentType,
+		},
+	}
+	for _, tc := range cases {
+		err := mainfluxSDK.ValidateContentType(tc.cType)
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", tc.desc, tc.err, err))
+	}
+}
