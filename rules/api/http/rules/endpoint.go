3cd8e7e1a242aebe5eb19fb1d8bce63d5c158332
Bogdan Mitrović <bogdan.mitrovic@mainflux.com>
2026-03-09 12:23:23 +0100

    extract http apis for rules and scripts into separate packages
    Signed-off-by: Bogdan Mitrović <bogdan.mitrovic@mainflux.com>

---
+++ rules/api/http/rules/endpoint.go
@@ -0,0 +1,235 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package rules
+
+import (
+	"context"
+
+	"github.com/MainfluxLabs/mainflux/pkg/apiutil"
+	"github.com/MainfluxLabs/mainflux/rules"
+	"github.com/go-kit/kit/endpoint"
+)
+
+func createRulesEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(createRulesReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		var rulesList []rules.Rule
+		for _, rReq := range req.Rules {
+			r := rules.Rule{
+				Name:        rReq.Name,
+				Conditions:  rReq.Conditions,
+				Operator:    rReq.Operator,
+				Actions:     rReq.Actions,
+				Description: rReq.Description,
+			}
+			rulesList = append(rulesList, r)
+		}
+
+		rules, err := svc.CreateRules(ctx, req.token, req.groupID, rulesList...)
+		if err != nil {
+			return nil, err
+		}
+
+		return buildRulesResponse(rules, true), nil
+	}
+}
+
+func listRulesByThingEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(listRulesByThingReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		page, err := svc.ListRulesByThing(ctx, req.token, req.thingID, req.pageMetadata)
+		if err != nil {
+			return nil, err
+		}
+
+		return buildRulesPageResponse(page, req.pageMetadata), nil
+	}
+}
+
+func listRulesByGroupEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(listRulesByGroupReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		page, err := svc.ListRulesByGroup(ctx, req.token, req.groupID, req.pageMetadata)
+		if err != nil {
+			return nil, err
+		}
+
+		return buildRulesPageResponse(page, req.pageMetadata), nil
+	}
+}
+
+func listThingIDsByRuleEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(ruleReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		ids, err := svc.ListThingIDsByRule(ctx, req.token, req.id)
+		if err != nil {
+			return nil, err
+		}
+
+		res := thingIDsRes{ThingIDs: ids}
+		return res, nil
+	}
+}
+
+func viewRuleEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(ruleReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		rule, err := svc.ViewRule(ctx, req.token, req.id)
+		if err != nil {
+			return nil, err
+		}
+
+		return buildRuleResponse(rule, false), nil
+	}
+}
+
+func updateRuleEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(updateRuleReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		rule := rules.Rule{
+			ID:          req.id,
+			Name:        req.Name,
+			Description: req.Description,
+			Conditions:  req.Conditions,
+			Operator:    req.Operator,
+			Actions:     req.Actions,
+		}
+
+		if err := svc.UpdateRule(ctx, req.token, rule); err != nil {
+			return nil, err
+		}
+
+		return ruleResponse{updated: true}, nil
+	}
+}
+
+func removeRulesEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(removeRulesReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		if err := svc.RemoveRules(ctx, req.token, req.RuleIDs...); err != nil {
+			return nil, err
+		}
+
+		return removeRes{}, nil
+	}
+}
+
+func assignRulesEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(thingRulesReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		if err := svc.AssignRules(ctx, req.token, req.thingID, req.RuleIDs...); err != nil {
+			return nil, err
+		}
+
+		return thingRulesRes{}, nil
+	}
+}
+
+func unassignRulesEndpoint(svc rules.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req := request.(thingRulesReq)
+		if err := req.validate(); err != nil {
+			return nil, err
+		}
+
+		if err := svc.UnassignRules(ctx, req.token, req.thingID, req.RuleIDs...); err != nil {
+			return nil, err
+		}
+
+		return thingRulesRes{}, nil
+	}
+}
+
+func buildRulesResponse(rules []rules.Rule, created bool) rulesRes {
+	res := rulesRes{Rules: []ruleResponse{}, created: created}
+
+	for _, r := range rules {
+		rule := ruleResponse{
+			ID:          r.ID,
+			GroupID:     r.GroupID,
+			Name:        r.Name,
+			Description: r.Description,
+			Conditions:  r.Conditions,
+			Operator:    r.Operator,
+			Actions:     r.Actions,
+		}
+		res.Rules = append(res.Rules, rule)
+	}
+
+	return res
+}
+
+func buildRulesPageResponse(page rules.RulesPage, pm apiutil.PageMetadata) RulesPageRes {
+	res := RulesPageRes{
+		pageRes: pageRes{
+			Total:  page.Total,
+			Offset: pm.Offset,
+			Limit:  pm.Limit,
+			Ord:    pm.Order,
+			Dir:    pm.Dir,
+			Name:   pm.Name,
+		},
+		Rules: []ruleResponse{},
+	}
+
+	for _, r := range page.Rules {
+		rule := ruleResponse{
+			ID:          r.ID,
+			GroupID:     r.GroupID,
+			Name:        r.Name,
+			Description: r.Description,
+			Conditions:  r.Conditions,
+			Operator:    r.Operator,
+			Actions:     r.Actions,
+		}
+		res.Rules = append(res.Rules, rule)
+	}
+
+	return res
+}
+
+func buildRuleResponse(rule rules.Rule, updated bool) ruleResponse {
+	return ruleResponse{
+		ID:          rule.ID,
+		GroupID:     rule.GroupID,
+		Name:        rule.Name,
+		Description: rule.Description,
+		Conditions:  rule.Conditions,
+		Operator:    rule.Operator,
+		Actions:     rule.Actions,
+		updated:     updated,
+	}
+}
--- rules/api/http/scripts/requests.go
+++ rules/api/http/scripts/requests.go
@@ -1,7 +1,7 @@
 // Copyright (c) Mainflux
 // SPDX-License-Identifier: Apache-2.0
 
-package http
+package scripts
 
 import (
 	"github.com/MainfluxLabs/mainflux/pkg/apiutil"
@@ -14,208 +14,6 @@ const (
 	maxNameSize   = 254
 	maxScriptSize = 65_535
 )
-
-type rule struct {
-	Name        string            `json:"name"`
-	Description string            `json:"description,omitempty"`
-	Conditions  []rules.Condition `json:"conditions"`
-	Operator    string            `json:"operator,omitempty"`
-	Actions     []rules.Action    `json:"actions"`
-}
-
-type createRulesReq struct {
-	token   string
-	groupID string
-	Rules   []rule `json:"rules"`
-}
-
-func (req createRulesReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.groupID == "" {
-		return apiutil.ErrMissingGroupID
-	}
-
-	if len(req.Rules) < minLen {
-		return apiutil.ErrEmptyList
-	}
-
-	for _, rule := range req.Rules {
-		if err := rule.validate(); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (req rule) validate() error {
-	if req.Name == "" || len(req.Name) > maxNameSize {
-		return apiutil.ErrNameSize
-	}
-
-	if len(req.Conditions) < minLen {
-		return apiutil.ErrEmptyList
-	}
-	for _, condition := range req.Conditions {
-		if condition.Field == "" {
-			return apiutil.ErrMissingConditionField
-		}
-		if condition.Comparator == "" {
-			return apiutil.ErrMissingConditionComparator
-		}
-		if condition.Threshold == nil {
-			return apiutil.ErrMissingConditionThreshold
-		}
-	}
-
-	if len(req.Conditions) > minLen {
-		if req.Operator != rules.OperatorAND && req.Operator != rules.OperatorOR {
-			return apiutil.ErrInvalidOperator
-		}
-	}
-
-	if len(req.Actions) < minLen {
-		return apiutil.ErrEmptyList
-	}
-	for _, action := range req.Actions {
-		switch action.Type {
-		case rules.ActionTypeSMTP, rules.ActionTypeSMPP:
-			if action.ID == "" {
-				return apiutil.ErrMissingActionID
-			}
-		case rules.ActionTypeAlarm:
-		default:
-			return apiutil.ErrInvalidActionType
-		}
-	}
-
-	return nil
-}
-
-type ruleReq struct {
-	token string
-	id    string
-}
-
-func (req ruleReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.id == "" {
-		return apiutil.ErrMissingRuleID
-	}
-
-	return nil
-}
-
-type listRulesByThingReq struct {
-	token        string
-	thingID      string
-	pageMetadata apiutil.PageMetadata
-}
-
-func (req listRulesByThingReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.thingID == "" {
-		return apiutil.ErrMissingThingID
-	}
-
-	return apiutil.ValidatePageMetadata(req.pageMetadata, maxLimitSize, maxNameSize)
-}
-
-type listRulesByGroupReq struct {
-	token        string
-	groupID      string
-	pageMetadata apiutil.PageMetadata
-}
-
-func (req listRulesByGroupReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.groupID == "" {
-		return apiutil.ErrMissingGroupID
-	}
-
-	return apiutil.ValidatePageMetadata(req.pageMetadata, maxLimitSize, maxNameSize)
-}
-
-type updateRuleReq struct {
-	token string
-	id    string
-	rule
-}
-
-func (req updateRuleReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.id == "" {
-		return apiutil.ErrMissingRuleID
-	}
-
-	return req.rule.validate()
-}
-
-type removeRulesReq struct {
-	token   string
-	RuleIDs []string `json:"rule_ids"`
-}
-
-func (req removeRulesReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if len(req.RuleIDs) < minLen {
-		return apiutil.ErrEmptyList
-	}
-
-	for _, ruleID := range req.RuleIDs {
-		if ruleID == "" {
-			return apiutil.ErrMissingRuleID
-		}
-	}
-
-	return nil
-}
-
-type thingRulesReq struct {
-	token   string
-	thingID string
-	RuleIDs []string `json:"rule_ids"`
-}
-
-func (req thingRulesReq) validate() error {
-	if req.token == "" {
-		return apiutil.ErrBearerToken
-	}
-
-	if req.thingID == "" {
-		return apiutil.ErrMissingThingID
-	}
-
-	if len(req.RuleIDs) < minLen {
-		return apiutil.ErrEmptyList
-	}
-
-	for _, ruleID := range req.RuleIDs {
-		if ruleID == "" {
-			return apiutil.ErrMissingRuleID
-		}
-	}
-
-	return nil
-}
 
 type script struct {
 	Name        string `json:"name"`
@@ -305,6 +103,10 @@ func (req scriptReq) validate() error {
 		return apiutil.ErrBearerToken
 	}
 
+	if req.id == "" {
+		return apiutil.ErrMissingScriptID
+	}
+
 	return nil
 }
 
@@ -317,6 +119,10 @@ type updateScriptReq struct {
 func (req updateScriptReq) validate() error {
 	if req.token == "" {
 		return apiutil.ErrBearerToken
+	}
+
+	if req.id == "" {
+		return apiutil.ErrMissingScriptID
 	}
 
 	if req.Name == "" || len(req.Name) > maxNameSize {
