275ee061bd21473b7a26aff5e096f8edde175998
majabirmancevic <majabi2001@gmail.com>
2026-05-28 10:32:41 +0200

    Fix subject naming
    Signed-off-by: majabirmancevic <majabi2001@gmail.com>

--- cmd/webhooks/main.go
+++ cmd/webhooks/main.go
@@ -142,7 +142,7 @@ func main() {
 		return subscribeToThingsES(ctx, svc, cfg, logger)
 	})
 
-	if err = consumers.Start(svcName, consumers.Messages(pubSub, svc, nats.SubjectWebhook)); err != nil {
+	if err = consumers.Start(svcName, consumers.Messages(pubSub, svc, nats.SubjectWebhooks)); err != nil {
 		logger.Error(fmt.Sprintf("Failed to create Webhook: %s", err))
 	}
 
