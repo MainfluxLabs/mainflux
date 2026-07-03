5b83ed76da7e232acbd02b00a2f857acd6ce5714
duolok <dusan.lecic5@gmail.com>
2026-07-03 10:01:37 +0200

    refactor: add checksum for postgres things

--- filestore/postgres/things.go
+++ filestore/postgres/things.go
@@ -29,8 +29,8 @@ func NewThingsRepository(db dbutil.Database) filestore.ThingsRepository {
 }
 
 func (tr thingsRepository) Save(ctx context.Context, thingID, groupID string, fi filestore.FileInfo) error {
-	q := `INSERT INTO things_files (file_name, file_class, file_format, thing_id, time, metadata, group_id)
-		VALUES (:file_name, :file_class, :file_format, :thing_id, :time, :metadata, :group_id)`
+	q := `INSERT INTO things_files (file_name, file_class, file_format, thing_id, time, metadata, group_id, checksum)
+		VALUES (:file_name, :file_class, :file_format, :thing_id, :time, :metadata, :group_id, :checksum)`
 
 	dbFile, err := toDBThingFile(thingID, fi)
 	if err != nil {
@@ -87,7 +87,7 @@ func (tr thingsRepository) Update(ctx context.Context, thingID string, fi filest
 }
 
 func (tr thingsRepository) Retrieve(ctx context.Context, thingID string, fi filestore.FileInfo) (filestore.FileInfo, error) {
-	q := `SELECT file_name, file_class, file_format, metadata FROM things_files
+	q := `SELECT file_name, file_class, file_format, metadata, checksum FROM things_files
 		WHERE thing_id = $1 AND file_class = $2 AND file_format = $3 AND file_name = $4`
 
 	dbFile := dbFileInfo{}
@@ -271,6 +271,7 @@ func toDBThingFile(thingID string, fi filestore.FileInfo) (dbThingFile, error) {
 			FileFormat: fi.Format,
 			Metadata:   meta,
 			Time:       fi.Time,
+			Checksum:   fi.Checksum,
 		},
 		ThingID: thingID,
 	}, nil
