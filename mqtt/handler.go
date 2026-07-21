ce3ec6a749a56d70179590cbf5bffa8bcc0bbfdf
duolok <dusan.lecic5@gmail.com>
2026-07-21 11:29:54 +0200

    fix: log

--- mqtt/handler.go
+++ mqtt/handler.go
@@ -207,7 +207,7 @@ func (h *handler) Publish(c *session.Client, topic *string, payload *[]byte) {
 	}
 
 	if err := h.publishToBus(c, *topic, *payload); err != nil {
-		h.logger.Error(fmt.Sprintf("client_id %s failed to publish to topic %s: %s", c.ID, *topic, err))
+		h.logger.Error(fmt.Sprintf("client_id %s failed to publish to topic %s", c.ID, *topic))
 		return
 	}
 
