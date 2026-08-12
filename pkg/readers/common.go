bc482d4c28189a3bf58de5458a314e7697e30b55
duolok <dusan.lecic5@gmail.com>
2026-08-12 09:44:51 +0200

    move: common files to pkg

---
+++ pkg/readers/common.go
@@ -0,0 +1,50 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package readers
+
+import (
+	"fmt"
+
+	"github.com/MainfluxLabs/mainflux/readers"
+)
+
+const (
+	JsonTable  = "json"
+	JsonOrder  = "created"
+	SenmlTable = "senml"
+	SenmlOrder = "time"
+)
+
+func BaseConditions(pm readers.ReadersMetadata, timeColumn string) []string {
+	var conds []string
+	if pm.Subtopic != "" {
+		conds = append(conds, "subtopic = :subtopic")
+	}
+	if pm.Publisher != "" {
+		conds = append(conds, "publisher = :publisher")
+	}
+	if pm.Protocol != "" {
+		conds = append(conds, "protocol = :protocol")
+	}
+	if pm.From != 0 {
+		conds = append(conds, fmt.Sprintf("%s >= :from", timeColumn))
+	}
+	if pm.To != 0 {
+		conds = append(conds, fmt.Sprintf("%s < :to", timeColumn))
+	}
+
+	return conds
+}
+
+func BaseQueryParams(pm readers.ReadersMetadata) map[string]any {
+	return map[string]any{
+		"limit":     pm.Limit,
+		"offset":    pm.Offset,
+		"subtopic":  pm.Subtopic,
+		"publisher": pm.Publisher,
+		"protocol":  pm.Protocol,
+		"from":      pm.From,
+		"to":        pm.To,
+	}
+}
