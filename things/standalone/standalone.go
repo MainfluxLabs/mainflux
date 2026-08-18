6c4b73522784d3d08f284b1b135c7ebc82655953
duolok <dusan.lecic5@gmail.com>
2026-08-18 08:58:33 +0200

    update: things stadalone

--- things/standalone/standalone.go
+++ things/standalone/standalone.go
@@ -35,6 +35,10 @@ func (repo singleUserRepo) Issue(ctx context.Context, id, email string, keyType 
 	return repo.token, nil
 }
 
+func (repo singleUserRepo) Refresh(ctx context.Context, refreshToken string) (string, error) {
+	return "", errUnsupported
+}
+
 func (repo singleUserRepo) Identify(ctx context.Context, token string) (domain.Identity, error) {
 	if repo.token != token {
 		return domain.Identity{}, errors.ErrAuthentication
