cf13813ae2ca221f3b806eb7612a8f4368ff01e5
duolok <dusan.lecic5@gmail.com>
2026-09-02 11:16:38 +0200

    fix: user id

--- auth/api/logging.go
+++ auth/api/logging.go
@@ -71,7 +71,7 @@ func (lm *loggingMiddleware) LogoutAll(ctx context.Context, token string) (err e
 
 func (lm *loggingMiddleware) RevokeSessions(ctx context.Context, userID string) (err error) {
 	defer func(begin time.Time) {
-		message := fmt.Sprintf("Method revoke_sessions for user %s took %s to complete", userID, time.Since(begin))
+		message := fmt.Sprintf("Method revoke_sessions for user id %s took %s to complete", userID, time.Since(begin))
 		if err != nil {
 			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
 			return
